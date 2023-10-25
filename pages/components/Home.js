e8986948093dbcc6ef54ed054a638389bfd57808
SalahBaksteen <9007168@student.zadkine.nl>
2023-10-25 19:44:32 +0200

    portfolio

---
+++ pages/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+           
+<section class="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
+    <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+        
+<h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Crafting Digital Dreams, <span class="underline underline-offset-3 decoration-8 decoration-purple-500 dark:decoration-purple-500">One Code at a Time</span></h1>
+<p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Where Imagination Embarks on a Journey of Infinite Innovation in the Realm of Web Development, Pioneering Tomorrow's Digital Experiences Today.</p>
+    </div>
+</section>
+ 
+        </div>
+    );
+
+};
+
+export default Home;
